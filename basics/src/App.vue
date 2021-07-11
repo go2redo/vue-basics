a45300af3b88b4b18a6d3140ee3fdc50cbf9b53b
Dmytro Kyrychuk <dmytro@kyrych.uk>
2021-07-11 14:06:30 +0300

    Create Agenda component

---
+++ basics/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Agenda :tasks="agenda" />
+</template>
+
+<script>
+import Agenda from "./components/Agenda.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      agenda: [
+        "Підготовка середовища розробки",
+        "Створення нового проекту за допомогою vue-cli",
+        "Базовий Vue",
+        "Розгортання проекту на Netlify",
+        "Наступні кроки.",
+      ],
+    };
+  },
+  components: {
+    Agenda,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ basics/src/components/Agenda.vue
@@ -0,0 +1,24 @@
+<template>
+  <ol>
+    <li v-for="item in tasks" :key="item">
+      {{ item }}
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: "Agenda",
+  props: {
+    tasks: Array,
+  },
+};
+</script>
+
+<style scoped>
+ol {
+  text-align: left;
+  width: 500px;
+  margin: 0 auto;
+}
+</style>
